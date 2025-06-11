4a9cbaf8f73285dffa3a403b3e2803576c0161c7
Tom D'Aquino <tdaquino@hiddenlayer.com>
2025-06-11 13:05:38 -0700

    Adding support for prompt dataset.

--- simple_prompt.py
+++ simple_prompt.py
@@ -68,11 +68,11 @@ class InvokeModel:
     def prompt(self, input):
 
         native_request = {
-            "max_tokens": 1024, 
+            # "max_tokens": 1024, 
             "temperature": 0.5,
             "prompt": f"<s>[INST] {input} [/INST]</s>",
-            "top_p": 0.7,
-            "top_k": 50,
+            # "top_p": 0.7,
+            # "top_k": 50,
         }
 
         request = json.dumps(native_request)
@@ -119,6 +119,8 @@ def main():
     parser.add_argument("--guardrail_id", help="Guardrail ID")
     parser.add_argument("--guardrail_version", help="Guardrail version")
     parser.add_argument("--log_file", help="Log file to save the conversation")
+    parser.add_argument("--prompts_file", help="File containing prompts to read from (optional)")
+    parser.add_argument("--summary_report", help="Generate a block/allow summary report (optional)")
 
     args = parser.parse_args()
     if args.guardrail_id and not args.guardrail_version:
@@ -130,25 +132,63 @@ def main():
     if args.proxy_url and args.guardrail_id:
         print("Both proxy URL and guardrail ID cannot be provided together.")
         sys.exit(1)
+    
+    interactive_mode = not args.prompts_file
+    if not interactive_mode and not args.log_file:
+        print("Log file is required for non-interactive mode.")
+        sys.exit(1)
 
     i = InvokeModel(**vars(args))
 
     print(f"Starting conversation with model {args.model_id}")
     print("Press Ctrl+C to exit")
     
-    while True:
+    if interactive_mode:
+        while True:
+            try:
+                user_input = None
+                while not user_input:
+                    user_input = input("Prompt: ")
+                    if not user_input:
+                        print("Prompt cannot be empty.\n")
+                response = i.prompt(user_input)
+                print(f"\nModel response: {response}\n")
+            except KeyboardInterrupt:
+                print("\nCtrl+C detected. Exiting...")
+                sys.exit()
+    else:
+        summary_report = {}
         try:
-            user_input = None
-            while not user_input:
-                user_input = input("Prompt: ")
-                if not user_input:
-                    print("Prompt cannot be empty.\n")
-            response = i.prompt(user_input)
-            print(f"\nModel response: {response}\n")
-        except KeyboardInterrupt:
-            print("\nCtrl+C detected. Exiting...")
-            sys.exit()
-
+            with open(args.prompts_file, "r") as f:
+                prompts = f.readlines()
+            for prompt in prompts:
+                prompt = prompt.strip()
+                prompt, label = prompt.split(",")
+                if not prompt:
+                    continue
+                response = i.prompt(prompt)
+                if "Message was blocked" in response:
+                    if label not in summary_report:
+                        summary_report[label] = {"allowed": 0, "blocked": 0, "total": 0}
+                    summary_report[label]["blocked"] += 1
+                else:
+                    if label not in summary_report:
+                        summary_report[label] = {"allowed": 0, "blocked": 0, "total": 0}
+                    summary_report[label]["allowed"] += 1
+                summary_report[label]["total"] += 1
+        except FileNotFoundError:
+            print(f"File not found: {args.prompts_file}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+        if args.summary_report:
+            print("\nSummary report...")
+            for k, v in summary_report.items():
+                print(f"\nLabel: {k}")
+                print(f"Total prompts: {v['total']}")
+                print(f"Allowed: {v['allowed']}")
+                print(f"Blocked: {v['blocked']}")
 if __name__ == "__main__":
     main()
 
